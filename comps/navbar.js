c98f57ed756cb96aea53998cb5c084f47b318533
Adi Kusuma <86455282+adiksmj@users.noreply.github.com>
2021-08-30 15:13:03 +0800

    Update navbar.js

--- comps/navbar.js
+++ comps/navbar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
-                <img src="/weeii2.png" width="130px" height ="130px"/>
+                <Image src="/weeii2.png" alt="weeii" width="130" height="130" />
             </div>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About Us</a></Link>
